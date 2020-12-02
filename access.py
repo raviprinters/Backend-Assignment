9194ebf6e1a35e04892dbcc4a42c9d7d426bf30a
VinithaThiagarajan <75349768+VinithaThiagarajan@users.noreply.github.com>
2020-12-02 10:31:36 +0530

    Update access.py

--- access.py
+++ access.py
@@ -8,10 +8,10 @@ y.create("vinitha",50)
 y.modify("vinitha",55)
 y.delete("vinitha")
 
-t1=Thread(target=(create or read or delete),args=(key_name,value,timeout))
+t1=Thread(target=(create or read or delete),args=(key_name,value,timeout)) #as per the operation
 t1.start()
 t1.sleep()
-t2=Thread(target=(create or read or delete),args=(key_name,value,timeout)) 
+t2=Thread(target=(create or read or delete),args=(key_name,value,timeout)) #as per the operation
 t2.start()
 t2.sleep()
 
